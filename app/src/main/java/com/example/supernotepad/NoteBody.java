19c12c4e9879a596eebbae226fc4e444471564c9
Guy rahamim <guy.danino5@gmail.com>
2020-10-04 19:58:37 +0300

    Added file loading basics
    To Do:
    -change the hash map to <int, file>
    - Add layouts programatically

--- app/src/main/java/com/example/supernotepad/NoteBody.java
+++ app/src/main/java/com/example/supernotepad/NoteBody.java
@@ -2,6 +2,8 @@ package com.example.supernotepad;
 
 //import class
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -54,7 +56,9 @@ public class NoteBody extends AppCompatActivity {
         View.OnClickListener backButtonListener = new View.OnClickListener(){
             public void onClick(View V){
           //implementation
-              finish();//Go back
+              //finish();//Go back
+                Intent intent =new Intent(NoteBody.this, MainActivity.class);
+                startActivity(intent);
             }
         };
         //create click listener for toggleCheckbox
